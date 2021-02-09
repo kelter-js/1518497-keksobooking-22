ea77364b3f5857619a5fb640a8dc7ce84562eb78
kelter-js <66162264+kelter-js@users.noreply.github.com>
2021-02-09 20:40:49 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,31 +3,46 @@ function getRandomNumber (from, to) {
   if (from == to) {
     return from;
   }
+
   if (to < from) {
-   console.log('Конечное значение диапазона меньше, чем его начало. Измените входные параметры.');
-   return;
- }
- const start = (from < 0) ? 0 : from;
- const end = (to < 0) ? 0 : to;
- return Math.floor(Math.random() * (Math.floor(end) - Math.ceil(start) + 1)) + Math.ceil(start);
+    console.log('Конечное значение диапазона меньше, чем его начало. Измените входные параметры.');
+    return;
+  }
+
+  if (from < 0 || to < 0) {
+    console.log('Конечное или начальное значения диапазона не могут быть отрицательными. Измените входные параметры.');
+    return;
+  }
+
+  return Math.floor(Math.random() * (Math.floor(to) - Math.ceil(from) + 1)) + Math.ceil(from);
 }
 
 function getRandomNumberFloat (from, to, amountDigitals) {
   if (from == to) {
     return from;
   }
-  if (to <= from) {
+
+  if (to < from) {
     console.log('Конечное значение диапазона меньше, чем его начало. Измените входные параметры.');
     return;
   }
-  const start = (from < 0) ? 0 : from;
-  const end = (to < 0) ? 0 : to;
-  const result = amountDigitals == 0 ? Math.trunc(start + Math.random() * (end - start)) : +(start + Math.random() * (end - start)).toFixed(amountDigitals);
-  if (result > end) {
-   const numberIntoString = String(end);
-   return +(numberIntoString.slice(0, numberIntoString.indexOf(".") + amountDigitals + 1));
+
+  if (from < 0 || to < 0) {
+    console.log('Конечное или начальное значения диапазона не могут быть отрицательными. Измените входные параметры.');
+    return;
+  }
+
+  const result = amountDigitals == 0 ? Math.trunc(from + Math.random() * (to - from)) : +(from + Math.random() * (to - from)).toFixed(amountDigitals);
+  if (result > to) {
+    const numberIntoString = String(to);
+    return +(numberIntoString.slice(0, numberIntoString.indexOf('.') + amountDigitals + 1));
   } else {
-   return result;
+    return result;
   }
 }
+
+getRandomNumber(0, 5);
+
+getRandomNumberFloat(1.51, 1.59, 1);
+
 /* eslint-enable */
